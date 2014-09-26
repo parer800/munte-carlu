3f4e9a54411c6b18e4fb611271bbabf12746df8e
Pär Eriksson <parer800@student.liu.se>
2014-09-26 14:29:31 +0200

    Added sphere intersection, not tested yet, must set up a test scenario to see if it works

--- src/sphere.py
+++ src/sphere.py
@@ -1,8 +1,8 @@
     #Sphere.py
 
 import numpy as np
-
-
+from numpy import linalg as LA
+from numpy import multiply as vecMult
 from geometry import Geometry
 from ray import Ray
 
@@ -25,6 +25,10 @@ class Sphere(Geometry):
     def setRadius(self, radius):
         self.radius = radius
 
+    #Set center point
+    def setCenterPoint(self, c):
+        self.centerPoint = c
+
     #================================================================
     #========================== GETTERS =============================
     #================================================================
@@ -33,21 +37,44 @@ class Sphere(Geometry):
     def getRadius(self):
         return self.radius
 
+    #Get center point
+    def getCenterPoint(self):
+        return self.centerPoint
+
 
     #================================================================
     #========================= VIRTUALS (Override) ==================
     #================================================================
 
     #Intersect sphere
-    def intersect(r):
+    def intersect(self, r):
+        """
+        Looks for intersections with a sphere for a ray
+        :param r: Ray object which includes an origin point and a direction
+        :return: False when ray has no intersection or tangent to the sphere, Returns intersection point as np.array if there is an intersection
+        """
         if isinstance(r, Ray) is True:
             #Check if ray really intersect with the sphere, i.e. has 2 insection points. Otherwise do not calculate the roots.
             #Ray : P = P0 + t*V
             #Sphere: |P-O|^2 = r^2 => ... => |P0 + t*V - O|^2 - r^2 = 0
             # Gives a quadratic equation at^2 + bt + c = 0
             # => ... => a = ||V||^2, b = 2*V(P0 - O), c = ||P0 - O||^2 - r^2
-            # Will result in t = -b +- sqrt((b/2)^2 - c)
-            b = 2 * np.dot(r.direction, r.origin)
+            # Will result in t = -b/2 +- sqrt((b/2)^2 - c) = -V(P0 - O +- sqrt((V(P0 - O))^2 - c)
+            a = 1 # since V is a unit vector => a = ||V||^2 = 1
+            b = 2 * np.dot(r.direction, r.origin - self.centerPoint)
+            c = LA.norm(r.origin - self.centerPoint)**2 - self.radius**2 #(**2 = ^2)
+            underSqrt = (b/2)**2 - a*c
 
-
-            print r.direction
+            if underSqrt < 0:
+                # No intersection, will not be any real roots
+                return False
+            elif underSqrt == 0:
+                # One intersection (tangent), only one solution but is neglected in this case
+                return False
+            else:
+                #Intersection with sphere!
+                squareroot = underSqrt**0.5
+                #We only want to know the intersection point closest to the camera; so select the lowest solution
+                t = -(b/2)/a - squareroot
+                intersection = t*vecMult(t, r.direction)
+                return intersection
